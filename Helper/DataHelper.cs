afd4fb6b7d8c2d848e5c1c842c28e323c801e81a
Luís Carlos G. <137448502+Luis-60@users.noreply.github.com>
2024-09-23 00:56:21 -0300

    Add files via upload

--- Helper/DataHelper.cs
+++ Helper/DataHelper.cs
@@ -21,6 +21,12 @@ namespace AulaAEDB01.Windows.Helper
        public static List<Idioma> ListaIdioma { get; set; }
 
        public static List<Usuario> ListaUsuario { get; set; }
+
+       public static List<Livro> ListaLivro { get; set; }
+
+       public static List<Tipo> ListaTipo { get; set; }
+    
+       public static List<AutorLivro> ListaAutorLivro { get; set; }
         public static SqlConnection Conexao()
         {
             string strconnection = "Data Source=DESKTOP-C9UNTLB\\SQLEXPRESS;Initial Catalog=biblioteca;Integrated Security=True;Connect Timeout=30;TrustServerCertificate=True;"; 
